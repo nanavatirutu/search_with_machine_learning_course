49b05bdce3a9309a432c3cbe6905d0834cf84d70
nanavatirutu <nanavati.rutu@gmail.com>
2022-03-17 07:37:15 +0000

    updated after run

--- week4/create_labeled_queries.py
+++ week4/create_labeled_queries.py
@@ -5,17 +5,19 @@ import pandas as pd
 import numpy as np
 import csv
 import nltk
+from nltk import word_tokenize
 import string
 from pathlib import Path
 
 
 # Useful if you want to perform stemming.
 import nltk
-stemmer = nltk.stem.SnowballStemmer()
+stemmer = nltk.stem.SnowballStemmer("english")
 
 categories_file_name = r'/workspace/datasets/product_data/categories/categories_0001_abcat0010000_to_pcmcat99300050000.xml'
 
 queries_file_name = r'/workspace/datasets/train.csv'
+output_file_name  = r'/workspace/datasets/labeled_query_data.txt'
 output_file_name_normalized = r'/workspace/datasets/normalized_query_data.txt'
 
 parser = argparse.ArgumentParser(description='Process arguments.')
@@ -50,8 +52,7 @@ def rollup_categories():
 def normalize_query(query):
     query_clean = "".join([i for i in query.lower() if i not in string.punctuation])
     query_clean_tokens = word_tokenize(query_clean)
-    sb = stemmer("english")
-    query_clean_tokens = [sb.stem(token) for token in query_clean_tokens]
+    query_clean_tokens = [stemmer.stem(token) for token in query_clean_tokens]
     return (" ").join(query_clean_tokens)
 
 if args.min_queries:
@@ -79,18 +80,19 @@ parents_df = pd.DataFrame(list(zip(categories, parents)), columns =['category', 
 # Read the training data into pandas, only keeping queries with non-root categories in our category tree.
 file_qn = Path(output_file_name_normalized)
 if file_qn.is_file():
-    df = pd.read_csv(output_file_name_normalized)[['categories','query']]
+    df = pd.read_csv(output_file_name_normalized)[['category','query']]
 else:
     df = pd.read_csv(queries_file_name)[['category', 'query']]
     df = df[df['category'].isin(categories)]
     df['normalised_query'] = df.apply(lambda x: normalize_query(x['query']), axis=1)
     df = df[df['normalised_query'] != '']
     df = df.filter(['category', 'normalised_query'], axis=1).rename(columns={'normalised_query': 'query'})
-    df.to_csv(output_file_name2, index=False)
+    df.to_csv(output_file_name_normalized, index=False)
 
 # IMPLEMENT ME: Convert queries to lowercase, and optionally implement other normalization, like stemming.
 rollup_categories()
 # IMPLEMENT ME: Roll up categories to ancestors to satisfy the minimum number of queries per category.
+df_counts = df.groupby('category').size().reset_index(name='count')
 
 # Create labels in fastText format.
 df['label'] = '__label__' + df['category']
